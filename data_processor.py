74de60b29136e21ff26378f58a97ab2747bbf453
Vanessa <vama3024@colorado.edu>
2022-10-26 13:00:25 -0600

    added function for 2D array

--- data_processor.py
+++ data_processor.py
@@ -1,7 +1,12 @@
-# remember to import your libraries!
+import numpy as np
 
 def get_random_matrix(num_rows, num_columns):
-	return None
+    array_2D = np.zeros((num_rows, num_columns))
+    array_2D[0][0] = 1
+    array_2D[0][1] = 2
+    array_2D[1][0] = 3
+    array_2D[1][1] = 4
+    return array_2D
 
 def get_file_dimensions(file_name):
 	return (0,0)
