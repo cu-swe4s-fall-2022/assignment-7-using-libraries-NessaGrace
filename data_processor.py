811c40c2133252205f2879f8fac469d4f3459316
Vanessa <vama3024@colorado.edu>
2022-10-26 15:53:03 -0600

    added error raising to fix bug

--- data_processor.py
+++ data_processor.py
@@ -2,12 +2,20 @@ import numpy as np
 
 def get_random_matrix(num_rows, num_columns, prime):
 
+    if type(num_rows) != int or type(num_columns) != int:
+        raise TypeError('incompatible data type')
+
+    if type(prime) != int:
+        raise TypeError('incompatible data type')
+
+    if num_rows <= 0 or num_columns <=0 or prime <= 0:
+        raise ValueError('all input parameters must be > 0')
+
     np.random.seed(prime)
 
     array_2D = np.random.rand(num_rows, num_columns)
 
     return array_2D
-    raise TypeError('incompatible data type')
 
 def get_file_dimensions(file_name):
 	return (0,0)
