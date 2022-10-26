b4261680383c8140fb6fe826922aeccd30a69c4f
Vanessa <vama3024@colorado.edu>
2022-10-26 15:26:17 -0600

    bug fixes and working test for np.random.rand

--- tests/test_data_processor.py
+++ tests/test_data_processor.py
@@ -15,41 +15,40 @@ class BaseTestCases:
 
 class TestDataProcessor(BaseTestCases.BaseTest):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.arr_2D = np.array([[1,2], [3,4]], dtype='f')
-        cls.arr_1D = np.array([1,2], dtype='f')
-        cls.arr_3D = np.array([[1,2], [3,4], [5,6]], dtype='f')
+    # note: class set up, tear down used for previous test versions
+#    @classmethod
+ #   def setUpClass(cls):
+  #      cls.arr_2D = np.array([[1,2], [3,4]], dtype='f')
+   #     cls.arr_1D = np.array([1,2], dtype='f')
+    #    cls.arr_3D = np.array([[1,2], [3,4], [5,6]], dtype='f')
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.arr_2D = None
-        cls.arr_1D = None
-        cls.arr_3D = None
 
     def test_get_random_matrix(self):
 
-        # test if 2-D array is produced
-        array2D_fcn = dp.get_random_matrix(2, 2)
-        np.testing.assert_array_equal(array2D_fcn, self.arr_2D)
-        np.testing.assert_raises(AssertionError,
-                                 np.testing.assert_array_equal,
-                                 array2D_fcn,
-                                 self.arr_1D)
-        np.testing.assert_raises(AssertionError,
-                                 np.testing.assert_array_equal,
-                                 array2D_fcn,
-                                 self.arr_3D)
+        # test if 2-D array is produced:
 
-        # test if random array is produced
         np.random.seed(7)
-        for i in range(5):
-            array_rand_test = np.concatenate([np.random.rand(i)])
-        #print(array_rand_test)
-        arr_unif_test = array_rand_test.reshape(2,2)
-        #print(arr_unif_test)
-        array_unif = dp.get_random_matrix(2,2)
+        array2D_fcn = dp.get_random_matrix(2, 2, 7)
+        fcn_size = array2D_fcn.size
+        arr2D_rand = np.random.rand(2, 2)
+        test_size = arr2D_rand.size
+        self.assertEqual(fcn_size, test_size)
+        arr1D_rand = np.random.rand(1, 1)
+        test2_size = arr1D_rand.size
+        self.assertNotEqual(fcn_size, test2_size)
+
+
+        # test if random array is produced:
+
+        np.random.seed(7)
+        arr_unif_test = np.random.rand(2, 2)
+        arr_unif_fail = np.random.rand(1, 1)
+        array_unif = dp.get_random_matrix(2, 2, 7)
         np.testing.assert_array_equal(arr_unif_test, array_unif)
+        np.testing.assert_raises(AssertionError,
+                                 np.testing.assert_array_equal,
+                                 arr_unif_fail,
+                                 array_unif)
 
 
 if __name__ == '__main__':
